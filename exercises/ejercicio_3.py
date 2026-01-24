ccc4ed7dcdb9818dd46d4b50b5e9a8ad8fe2c1d3
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2026-01-24 15:19:12 -0500

    Algoritmo Sliding Window

--- exercises/ejercicio_3.py
+++ exercises/ejercicio_3.py
@@ -16,6 +16,28 @@ Ejemplo 3
     resultado → 3   ("wke")
 """
 
+s = "abcabcbb"
+
 
 def sliding(cadena):
-    pass
+    if len(cadena) == 0:
+        return 0
+
+    max_long = 0
+    izq = 0
+    set_cadena = set()
+
+    for der in range(len(cadena)):
+        while cadena[der] in set_cadena:
+            set_cadena.discard(cadena[izq])
+            izq += 1
+
+        set_cadena.add(cadena[der])
+        print(set_cadena)
+        max_long = max(max_long, der - izq + 1)
+
+    return max_long
+
+
+result = sliding(s)
+print(result)
